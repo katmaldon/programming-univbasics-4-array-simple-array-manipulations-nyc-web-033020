1a9883d4eac33702ab76110bdeb51238be217b04
Kat Maldon <katharinamaldon@gmail.com>
2020-03-26 17:34:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,3 +26,22 @@ def using_concat(arr1, arr2)
   array1.concat(arr2)
 end
 
+def using_insert(array, element)
+  array.insert(element)[4]
+end 
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten
+end 
+
+def using_delete(array, string)
+  array.delete(string)
+end 
+
+def using_delete_at(array, n)
+  array.delete[n]
+end
