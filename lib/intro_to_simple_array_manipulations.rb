ffea9a5490d81dc2c01196b82b766d12d462ecac
Kat Maldon <katharinamaldon@gmail.com>
2020-03-26 17:26:16 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array)
+  array.unshift
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array, string)
+  array.pop(string)
+end
+
+def using_shift
