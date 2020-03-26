d273113da79cab1911395bee5d216f8dbdc90bc0
Kat Maldon <katharinamaldon@gmail.com>
2020-03-26 17:29:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,16 +2,27 @@ def using_push(array, string)
   array.push(string)
 end
 
-def using_unshift(array)
-  array.unshift
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
 def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array, string)
-  array.pop(string)
+def pop_with_args(array, n)
+  array.pop(n)
 end
 
-def using_shift
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array, n)
+  array.shift(n)
+end
+
+def using_concat(arr1, arr2)
+  array1.concat(arr2)
+end
+
